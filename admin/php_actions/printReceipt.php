6b3afbbb2676a69323a96f37d8b567ad00a1f2c3
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2021-03-15 10:00:56 +0800

    changed connection credentials

--- admin/php_actions/printReceipt.php
+++ admin/php_actions/printReceipt.php
@@ -1,8 +1,8 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "thesis";
+    $db_server = "sql6.freemysqlhosting.net";
+    $db_user = "sql6398227";
+    $db_pass = "e68kqdz6FX";
+    $db_name = "sql6398227";
 
     $conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
 
